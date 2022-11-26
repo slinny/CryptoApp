b15bf07fd7211891310f156f09331ee9bc8d00bd
siranli <siranli411@gmail.com>
2022-11-25 23:39:06 -0600

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Siran Li on 11/25/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
