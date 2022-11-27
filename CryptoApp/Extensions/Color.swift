d6409fc4046891dbaeba70586870064d39b50938
siranli <siranli411@gmail.com>
2022-11-26 22:53:01 -0600

    added ui, api model

---
+++ CryptoApp/Extensions/Color.swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  CryptoApp
+//
+//  Created by Siran Li on 11/26/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
