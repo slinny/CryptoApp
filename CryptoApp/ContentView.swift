bb56bd61030178fea7adf3cb03621b01cd679a89
siranli <siranli411@gmail.com>
2022-12-01 00:34:58 -0600

    added list view for portfolio and all coins.

--- CryptoApp/ContentView.swift
+++ CryptoApp/ContentView.swift
@@ -9,18 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Text("Accent Color")
+                    .foregroundColor(Color.theme.accent)
+                
+                Text("Secondary Text Color")
+                    .foregroundColor(Color.theme.secondaryText)
+                
+                Text("Red Color")
+                    .foregroundColor(Color.theme.red)
+                
+                Text("Green Color")
+                    .foregroundColor(Color.theme.green)
+            }
         }
-        .padding()
+        
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
+//        }
+//        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
